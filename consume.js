66379e1baa1d41e1fa9c4d4b0fc395c3a810fc89
Eduardo Dênis Paiva Whitehurst <119756830+eduardo-whitehurst@users.noreply.github.com>
2025-03-02 11:24:39 -0300

    Corrigindo o consume.js

--- consume.js
+++ consume.js
@@ -2,22 +2,27 @@ const Redis = require("ioredis");
 const mongoose = require("mongoose");
 
 // Conexão com o MongoDB
-mongoose.connect("mongodb://mongodb:27017/mydatabase", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB conectado no consumidor"))
+mongoose
+  .connect("mongodb://mongodb:27017/mydatabase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB conectado no consumidor"))
   .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
 
 // Modelo do MongoDB
 const DataSchema = new mongoose.Schema({
-  id: Number,
-  name: String,
-  timestamp: { type: Date, default: Date.now },
+  id: Number, // Identificador único
+  name: String, // Nome da tarefa
+  timestamp: { type: Date, default: Date.now }, // Carimbo de data/hora
 });
 const DataModel = mongoose.model("Data", DataSchema);
 
 // Configuração do Redis
-const redis = new Redis();
+const redis = new Redis({
+  host: "redis", // Nome do serviço Redis definido no docker-compose
+  port: 6379, // Porta padrão do Redis
+});
 redis.on("connect", () => console.log("Redis conectado no consumidor!"));
 redis.on("error", (err) => console.error("Erro ao conectar ao Redis:", err));
 
@@ -25,10 +30,11 @@ redis.on("error", (err) => console.error("Erro ao conectar ao Redis:", err));
 const consumeMessage = async (queue) => {
   while (true) {
     try {
-      const message = await redis.blpop(queue, 0); // Aguarda até receber uma mensagem
-      const data = JSON.parse(message[1]);
+      // Aguarda até receber uma mensagem
+      const message = await redis.blpop(queue, 0);
+      const data = JSON.parse(message[1]); // Converte a mensagem de string para JSON
 
-      // Processar a mensagem e salvar no MongoDB
+      // Processa a mensagem e salva no MongoDB
       const newData = new DataModel(data.data); // Salva apenas o campo "data"
       await newData.save();
       console.log("Mensagem processada e salva no MongoDB:", data);
